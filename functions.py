487bd95ed8faf2e47cf25a06ba0e905e3cd3465c
Rish Dass <rishdass6@gmail.com>
2026-07-01 19:39:16 -0500

    Added Evaluation functions

--- functions.py
+++ functions.py
@@ -3,6 +3,10 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
 from bert_score import score
+import torch
+import torch.nn.functional as F
+from transformers import AutoTokenizer, AutoModel
+import code_bert_score
 
 def text_vectorize_score(prediction: str, g_truth: str):
     # Cosine Similarity
@@ -19,7 +23,14 @@ def text_vectorize_score(prediction: str, g_truth: str):
 
     # BERTScore
 
-    P, R, F1 = score(prediction, g_truth, lang="en", verbose = False, model_type="distilbert-base-uncased")
+    pred_input = [prediction]
+    truth_input = [g_truth]
+
+    P, R, F1 = score(pred_input, truth_input, lang="en", verbose = False, model_type="distilbert-base-uncased")
+
+    P = P.item()
+    R = R.item()
+    F1 = F1.item()
 
     return {
         "cosine_similarity": cos_similarity,
@@ -28,8 +39,63 @@ def text_vectorize_score(prediction: str, g_truth: str):
         "BERTScore_R": R 
     }
 
+tokenizer = AutoTokenizer.from_pretrained("microsoft/unixcoder-base")
+model = AutoModel.from_pretrained("microsoft/unixcoder-base")
+
 def code_vectorize_score(prediction: str, g_truth: str):
-    pass
+
+    #Cosine Similarity for Code
+    def get_code_embedding(code_snippet):
+        tokens = tokenizer(
+            f"<encoder-only> {code_snippet}",
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=512
+        )
+
+        with torch.no_grad():
+            outputs = model(**tokens)
+
+        embeddings = outputs.last_hidden_state
+        attention_mask = tokens['attention_mask'].unsqueeze(-1)
+
+        masked_embeddings = embeddings * attention_mask
+        sum_embeddings = torch.sum(masked_embeddings, dim = 1)
+        sum_mask = torch.clamp(attention_mask.sum(dim=1), min=1e-9)
+
+        return sum_embeddings / sum_mask
+    
+    vector_pred = get_code_embedding(prediction)
+    vector_truth = get_code_embedding(g_truth)
+
+    similarity = F.cosine_similarity(vector_truth, vector_pred)
+
+    similarity = similarity.item()
+
+    #BERTScore for Code
+    pred_input = [prediction]
+    truth_input = [g_truth]
+
+    P, R, F1, F3 = code_bert_score.score(
+        cands = pred_input,
+        refs = truth_input,
+        model_type = "microsoft/unixcoder-base",
+        lang = "python"
+    )
+
+    P1 = P.item()
+    R1 = R.item()
+    F1_1 = F1.item()
+
+    return {
+        "cosine_similarity": similarity,
+        "BERTScore_F1": F1_1,
+        "BERTScore_P": P1,
+        "BERTScore_R": R1 
+    }
 
 def data_completion_score(prediction: float, g_truth: float):
-    pass
+    #Simple MSE
+
+    return (g_truth - prediction) ** 2
